dcc7c448da3a396bcdf057261fac56662a93f8f0
baraka86538 <55498368+baraka86538@users.noreply.github.com>
2019-09-18 10:20:51 -0400

    version 1

---
+++ 115barakakagimbi.py
@@ -0,0 +1,21 @@
+#   a115_buggy_image.py
+import turtle as trtl
+
+body = trtl.Turtle()
+body.pensize(40)
+body.circle(20)
+'''
+w = 6
+y = 70
+z = 380 / ll
+x.pensize(5)
+n = 0
+while (n < ll):
+  body.goto(0,0)
+  body.setheading(z*n)
+  body.forward(y)
+  n = n + 1
+x.hideturtle()
+'''
+wn = trtl.Screen()
+wn.mainloop()
